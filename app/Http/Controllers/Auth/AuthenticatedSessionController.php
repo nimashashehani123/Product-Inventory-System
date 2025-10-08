b82e25984a2794035eb83614aaccdc97ea1ca26d
T.L. Nimasha Shehani <nimashashehani0715@gmail.com>
2025-10-08 19:39:19 +0530

    change login redirect based on user type

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,11 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+    if (auth()->user()->type === 'admin') {
+        return redirect()->route('admin.dashboard');
+    } else {
+        return redirect()->route('dashboard');
+    }
     }
 
     /**
